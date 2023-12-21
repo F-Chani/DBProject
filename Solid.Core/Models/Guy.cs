2e8f3d88e2419d35e8d8cb16137bdd28f354a342
Shani325 <113825550+Shani325@users.noreply.github.com>
2023-12-21 14:44:27 +0200

    first commit

---
+++ Solid.Core/Models/Guy.cs
@@ -0,0 +1,14 @@
+﻿namespace Solid.Core.Models
+{
+    public class Guy
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public double Heigh { get; set; }
+        public string Yeshiva { get; set; }
+        public string Sector { get; set; }
+        public bool IfGiveFlat { get; set; }
+
+    }
+}
